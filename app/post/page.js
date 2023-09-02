54d13ab3ef6eeacdc7803d8360da1785b3b5ab0f
jrubow <jrubow@purdue.edu>
2023-09-02 16:05:42 -0400

    Update 9/2/23 from Skywalker

--- app/post/page.js
+++ app/post/page.js
@@ -4,7 +4,9 @@ import styles from "./page.module.css"
 function page() {
   return (
     <div className={styles.main}>
-
+      <div className={styles.header}>
+        <h2>Post Sighting!</h2>
+      </div>
     </div>
   )
 }
