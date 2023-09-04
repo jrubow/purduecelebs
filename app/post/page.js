b09be4fcf9e6ed0082b5bc38c9d496945d0a304e
jrubow <jrubow@purdue.edu>
2023-09-04 16:09:52 -0400

    update

--- app/post/page.js
+++ app/post/page.js
@@ -1,20 +1,41 @@
 "use client";
 
 import React from 'react'
+import {useState} from 'react'
 import styles from "./page.module.css"
 
 function page() {
+  const [step, setStep] = useState(0);
+
   return (
     <div className={styles.main}>
       <div className={styles.header}>
         <div className={styles.headerBackground}></div>
         <p className={styles.headerText}>Post Sighting!</p>
       </div>
-      
-      <div className="form"> 
-        <input></input>
+      {
+        step == 0 ? 
+          <div className="form"> 
+            <input></input>
+          </div>
+        : step == 1 ? 
+          <div className={styles.step1}>
+            
+          </div>
+        : step == 2 ? 
+          <div className={styles.step2}> 
 
-      </div>
+          </div>
+        : step == 3 ? 
+          <div className={styles.step3}>
+
+          </div>
+        : 
+          <div className={styles.step4}>
+
+          </div>
+          
+      }
     </div>
   )
 }
